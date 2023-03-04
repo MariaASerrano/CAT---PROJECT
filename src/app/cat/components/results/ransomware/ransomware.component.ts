96395809409f7812c4cb66cc9afceda9eb1c90bc
MariaASerrano <angisemo08@hotmail.com>
2023-03-03 22:47:41 -0500

    ransomware-cat

--- src/app/cat/components/results/ransomware/ransomware.component.ts
+++ src/app/cat/components/results/ransomware/ransomware.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  ApexAxisChartSeries,
+  ApexTitleSubtitle,
+  ApexChart,
+  ApexYAxis,
+  ApexLegend,
+  ApexXAxis,
+  ApexPlotOptions,
+  ApexDataLabels,
+  ApexStroke,
+  ApexFill,
+  ApexNonAxisChartSeries,
+} from 'ng-apexcharts';
 @Component({
   selector: 'app-ransomware',
   templateUrl: './ransomware.component.html',
@@ -7,9 +19,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RansomwareComponent implements OnInit {
 
+    //radial bar
+    series_bar: ApexNonAxisChartSeries = [];
+    chart_bar: ApexChart = { type: 'radialBar' };
+    labels_bar: string[] = [];
+    plotOptions_bar: ApexPlotOptions = {};
+    fill_bar: ApexFill = {};
+    stroke_bar: ApexStroke = {};
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeChartOptions();
   }
-
-}
+  private initializeChartOptions(): void {
+    //radial bar
+  this.series_bar = [30]; //aquí va el porcentaje de preparación
+  (this.chart_bar = {
+    height: 350,
+    type: 'radialBar',
+    toolbar: {
+      show: true,
+    },
+  }),
+    (this.plotOptions_bar = {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24,
+          },
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35,
+          },
+        },
+  
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: 'black',
+            fontSize: '20px',
+            fontFamily:
+              "Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+          },
+          value: {
+            formatter: function (val) {
+              return parseInt(val.toString(), 10).toString();
+            },
+            color: '#black',
+            fontSize: '40px',
+            show: true,
+          },
+        },
+      },
+    }),
+    (this.fill_bar = {
+      type:"stroke",
+    }),
+    (this.stroke_bar = {
+      lineCap: 'round',
+    }),
+    (this.labels_bar = ['Preparación']);
+     
+    }
+  }
