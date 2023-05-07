8daf91c5ccfa79a230bc49751b7102565b29ca0f
OrlandoMR <orlandomoncada610@gmail.com>
2023-05-06 20:34:11 -0500

    update services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl:'https://back.flowriver.online/api/'
-  apiUrl:'http://localhost:3000/api/'
+  // apiUrl:'http://localhost:3000/api/',
+  apiUrl: 'https://cat-backend.herokuapp.com/api/',
 };
